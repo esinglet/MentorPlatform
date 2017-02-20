1294fee26c1b9950105ff954811e1a980a41a0fc
Evan Singleton <esinglet@ualberta.ca>
2017-02-20 02:22:39 -0700

    v

---
+++ config/config.js
@@ -0,0 +1,9 @@
+{
+  "aws":{"connectionLimit": 100,
+    "host": "odyssey.c3aiprsfywzf.us-west-2.rds.amazonaws.com",
+    "port": "3306",
+    "user": "root",
+    "password": "kV7UM275msrT",
+    "database": "odyssey_dev",
+    "debug":  false}
+}
