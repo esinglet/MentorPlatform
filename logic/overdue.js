1c57bf0851e10aadca157efddce931ebd60f2802
Evan Singleton <esinglet@ualberta.ca>
2017-04-17 17:00:04 -0600

    cleaning up

--- logic/overdue.js
+++ logic/overdue.js
@@ -2,42 +2,46 @@ var cron = require("node-cron");
 var db = require("../database/database.js");
 var email = require("email");
 
-email.
 
 function testOverdue(rate, dif){
     var range = rate * 7;
     console.log(range);
     if(dif >= range){
-        /* Send email */
-        /* Incriment counter */
-        /* Test if admin notified */
+        return true;
+    } else{
+        return false;
     }
 }
 
-var sendEmail = db.getRelationships(function(err, data){
-            if(err){
-                throw err;
-            } 
-            console.log(data);
-            var curDate = new Date();
 
-            for (r in data){
-                var rel = data[r];
-                if(rel.date_met){
-                    var date = rel.date_met;
-                } else{
-                    var date = rel.date_created;
-                }
-                var dif = (curDate.getTime() - Date.parse(date))/(60*60*24*1000);
-                console.log(dif);
-                testOverdue(rel.rate, dif);
-            }
-    });
 
 
 // http://www.nncron.ru/help/EN/working/cron-format.htm
 cron.schedule("50 20 * * *", function(){
-    sendEmail();
+    db.getRelationships(function(err, data){
+            if(err){
+                //Log error TODO
+                throw err;
+            } 
+            var curDate = new Date();
+
+            data.map(function(rel){
+                let date;
+                if(rel.date_met){
+                    date = rel.date_met;
+                } else{
+                    date = rel.date_created;
+                }
+                let dif = (curDate.getTime() - Date.parse(date))/(60*60*24*1000);
+
+                console.log(dif);
+                if(testOverdue(rel.rate, dif)){
+                    //Send email
+                    //increment
+                    //if count = 3 send to emily
+                }
+            });
+    });
 });
 
 
