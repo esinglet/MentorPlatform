6a5ef69cf1dbfab3953a96a69d8d7c20467227e7
MattDekinder <matthew.dekinder@gmail.com>
2017-04-17 19:30:36 -0600

    promise

--- logic/overdue.js
+++ logic/overdue.js
@@ -20,48 +20,50 @@ cron.schedule("* * * * * *", function(){
                 //Log error TODO
                 throw err;
             }
-            var admins = db.getAdmins();
+            db.getAdmins().then(function(admins){
+                var curDate = new Date();
 
-            var curDate = new Date();
-
-            data.map(function(rel){
-                let date;
-                if(rel.date_met){
-                    date = rel.date_met;
-                } else{
-                    date = rel.date_start;
-                }
-                let dif = (curDate.getTime() - Date.parse(date))/(60*60*24*1000);
-
-                console.log(dif);
-                if(testOverdue((rel.email_count!=0)? rel.rate*rel.email_count :rel.rate, dif)){
-                    try {
-                        //Send email
-                        //email.sendEmailMesg([rel.email], 'Your Odyssey Mentorship Survey Ready', ''); //todo: get token link for survey
-                        //console.log('about to sent an email:'+rel.menteeemail);
-                        email.sendEmailMesg([rel.menteeemail], 'Your Odyssey Mentorship Survey Ready', 'email body');
-                        //console.log('email away');
-
-                        //increment email count
-                        db.incrimentRelationship(rel.relid, function(e, ret){
-                            if (e)
-                                throw e;
-                        });
-
-                        //send emails with multiple of 3 to the admin
-                        if ((rel.email_count+1)%3 === 0){
-                            let subject = `${rel.menteefname}, ${rel.menteelname} is late with their Odyssey Mentorship Survey`;
-                            let body = `Someone is late with their Odyssey Mentorship Survey`;
-                            console.log(admins);
-                            console.log([admins[rel.org].email]);
-                            email.sendEmailMesgendEmail([admins[rel.org].email], subject, body);
-                        }
-                    } catch (e){
-                        //todo: any email errors should be handled here
-                        throw e;
+                data.map(function(rel){
+                    let date;
+                    if(rel.date_met){
+                        date = rel.date_met;
+                    } else{
+                        date = rel.date_start;
                     }
-                }
+                    let dif = (curDate.getTime() - Date.parse(date))/(60*60*24*1000);
+
+                    console.log(dif);
+                    if(testOverdue((rel.email_count!=0)? rel.rate*rel.email_count :rel.rate, dif)){
+                        try {
+                            //Send email
+                            //email.sendEmailMesg([rel.email], 'Your Odyssey Mentorship Survey Ready', ''); //todo: get token link for survey
+                            //console.log('about to sent an email:'+rel.menteeemail);
+                            email.sendEmailMesg([rel.menteeemail], 'Your Odyssey Mentorship Survey Ready', 'email body');
+                            //console.log('email away');
+
+                            //increment email count
+                            db.incrimentRelationship(rel.relid, function(e, ret){
+                                if (e)
+                                    throw e;
+                            });
+
+                            //send emails with multiple of 3 to the admin
+                            if ((rel.email_count+1)%3 === 0){
+                                let subject = `${rel.menteefname}, ${rel.menteelname} is late with their Odyssey Mentorship Survey`;
+                                let body = `Someone is late with their Odyssey Mentorship Survey`;
+                                console.log(admins);
+                                console.log([admins[rel.org].email]);
+                                email.sendEmailMesgendEmail([admins[rel.org].email], subject, body);
+                            }
+                        } catch (e){
+                            //todo: any email errors should be handled here
+                            throw e;
+                        }
+                    }
+                });
             });
+
+
     });
 });
 
